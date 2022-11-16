15fc1a2f4acea4ab9520ffc144d9cced4fa51e9b
NicolasSuarez11 <est.nicolas.suarez@unimilitar.edu.co>
2022-11-15 21:17:09 -0500

    agh

--- app.js
+++
@@ -1,8 +0,0 @@
-import { login, logout } from "./auth.js";
-const buttonLogin = document.querySelector("#button-login");
-
-buttonLogin.addEventListener("click", async (e) => {
-    try {
-        await login();
-    } catch (error) {}
-}); 
